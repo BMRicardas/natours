a20b35225da7aab67b676b73d6e7c81142ee4556
Ričardas Brazdžius <ricardas.brazdzius@gmail.com>
2025-04-17 23:12:04 +0300

    add CORS middleware for handling cross-origin requests

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -22,6 +23,10 @@ app.set('view engine', 'pug');
 app.set('views', `${__dirname}/views`);
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+
+app.options('*', cors());
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 // Set security HTTP headers
